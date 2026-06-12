73ab969fd8f8c097d9e2cfe8b376503c660dad05
Mau439Gs <124903930+Mau439Gs@users.noreply.github.com>
2026-06-12 15:15:36 -0600

    feat: añadido de busqueda y obtencion del catalogo de productos

--- logica/gestor_productos.py
+++ logica/gestor_productos.py
@@ -18,3 +18,11 @@ class GestorProductos:
     def eliminar_producto(self, id_producto):
 
         return self.db.eliminar_producto(id_producto)
+
+    def obtener_catalogo(self) -> list:
+        
+        return self.db.obtener_catalogo()
+
+    def buscar_producto(self, termino: str) -> list:
+        termino_limpio = self.normalizar_texto(termino)
+        return self.db.buscar_productos(termino_limpio)
